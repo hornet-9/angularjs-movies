1769501f5d0f50a1a056d89aab789d079cd808ef
hornet-9 <benkhanna@hotmail.com>
2019-12-10 22:29:42 +0100

    App new api key

--- app/js/services.js
+++ app/js/services.js
@@ -8,17 +8,17 @@ factory('moviesAPIservice', function($http) {
     moviesAPI.getMovies = function(date, offset) {
         const daysInMonth = new Date(date.substring(0,4), date.substring(5,7), 0).getDate();
         const datePeriod = date + '-01;' + date + '-' + daysInMonth;
-        return $http({ method: 'GET', url: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=292b053901534f9fafd16f72f1d017e0&order=by-title&opening-date=' + datePeriod + '&offset=' + offset});
+        return $http({ method: 'GET', url: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=L7YZGa8N4GrfDcoicrL4ICAqLTaQQgJj&order=by-title&opening-date=' + datePeriod + '&offset=' + offset});
     }
     
     moviesAPI.getMovie = function(opening_date, title) {
         // Include opening date (if it exists) to avoid other movies with same name
         const dateSearch = !opening_date ? '&opening-date=' + opening_date : '';
-            return $http({ method: 'GET', url: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=292b053901534f9fafd16f72f1d017e0&query=\'' + title + '\'' + dateSearch});
+            return $http({ method: 'GET', url: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=L7YZGa8N4GrfDcoicrL4ICAqLTaQQgJj&query=\'' + title + '\'' + dateSearch});
     }
 
     moviesAPI.movieSearch = function(searchText, offset) {
-        return $http({ method: 'GET', url: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=292b053901534f9fafd16f72f1d017e0&order=by-title&query=' + searchText + '&offset=' + offset});
+        return $http({ method: 'GET', url: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=L7YZGa8N4GrfDcoicrL4ICAqLTaQQgJj&order=by-title&query=' + searchText + '&offset=' + offset});
     }
 
     return moviesAPI;
